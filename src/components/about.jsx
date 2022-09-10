ac0d063c08c938b7c108b50048b0bbe04ef37320
climax-solution <blocklimax@gmail.com>
2022-09-10 18:54:59 +0300

    update about

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -17,7 +17,7 @@ export default function About() {
                 >
                     <p>Kouta-Kun Inu is a community and like minded driven project. Over the years and  recent times, we have had  devs rug project and made away with peoples hard earned.</p>
                     <p>So, a group of fellas came together to give back trust and build a sustainable community.</p>
-                    <p>Kouta-kun inu is a deflationary Token/NFT projects that provides a decentralized financial asset which rewards users with 8% Bnb rewards hourly directly to their wallets automatically.</p>
+                    <p>Kouta-kun inu is a reflectionary Token/NFT projects that provides a decentralized financial asset which rewards users with 8% Bnb rewards hourly directly to their wallets automatically.</p>
                     <p>The interest and safety of our investors is our top most priority. Thereby providing for them a top notch tokenomics.</p>
                     <p>Moreso, our NFT project provide our investors with passive income, in form of staking. Earning 2.5% of kouta-kun- Inu which can be converted to BNB when NFT is staked.</p>
                     <p>Building market place. There shall be our own NFT market place for the launch of our NFT.</p>
